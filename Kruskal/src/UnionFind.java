38c44bd39d8ad40c50f317c8677ada201edff62d
dnjsdud <dnjsdud9718@naver.com>
2022-02-15 12:31:46 +0900

    Kruskal Commit

--- Kruskal/src/UnionFind.java
+++ Kruskal/src/UnionFind.java
@@ -1,4 +1,14 @@
 public class UnionFind {
+    class Node {
+        private int parent, rank;
+        public Node(int p, int r){
+            parent = p; rank = r;
+        }
+        public void setParent(int p){parent = p;}
+        public void setRank(int r){rank = r;}
+        public int getParent(){return parent;}
+        public int getRank(){return rank;}
+    }
     private Node [] a;
     public UnionFind(int N){
         a = new Node[N];
